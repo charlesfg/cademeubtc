2380f33b0c4eab0e0076abc777e65f041ba1dcd2
Charles Goncalves <charles.ferreira@miningmath.com>
2017-06-11 12:24:20 -0300

    Class to watch bitcoin Wallet

---
+++ BTCWalletWatcher.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: charles
+ * Date: 10/06/17
+ * Time: 12:32
+ */
+class BTCWalletWatcher
+{
+    private $addr;
+
+
+    function __construct($addr) {
+        $this->addr = $addr;
+    }
+
+    private function get($url){
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+        $content = curl_exec($ch);
+        curl_close($ch);
+        return $content;
+    }
+
+    public function satoshi_to_btc($sat){
+        return $sat / (float) 100000000;
+    }
+
+    public function btc_usd(){
+        return $this->btc_ask("https://www.bitstamp.net/api/v2/ticker/btcusd/");
+    }
+
+    public function btc_ask($url, $which = "last"){
+        $json_ret = $this->get($url);
+        $ret = json_decode($json_ret, true);
+        return $ret[$which];
+    }
+
+    public function btc_brl(){
+        return $this->btc_ask("https://api.blinktrade.com/api/v1/BRL/ticker");
+    }
+
+    public function getWalletBalance(){
+
+        $satoshi_value = $this->get("https://blockchain.info/q/addressbalance/" . $this->addr);
+        $btc_value = $this->satoshi_to_btc($satoshi_value);
+        $brl_value = $btc_value * $this->btc_brl();
+        $usd_value = $btc_value * $this->btc_usd();
+
+        $balance = array($btc_value, $brl_value, $usd_value);
+
+        return $balance;
+    }
+}
